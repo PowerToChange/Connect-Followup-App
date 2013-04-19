b98afdaa42485c94f4e299e5ba3229f4c593c34f
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-04-19 16:37:43 -0400

    Add screen 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def link_to_icon(icon, url, options = {})
-    link_to %(<i class="icon-#{icon}"></i>).html_safe, url, options
+    link_to %(<i class="icon-#{icon} icon-large"></i>).html_safe, url, options
   end
 end
