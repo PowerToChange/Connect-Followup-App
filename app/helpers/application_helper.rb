48de353ba57995fb299b2b0f696c36334521d54b
Adrian Teh <adrian@codespore.com>
2013-06-05 12:55:16 -0400

    AT-51103163-User should be able to call & send sms by clicking a link/button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def link_to_icon(icon, url, options = {})
     link_to %(<i class="icon-#{icon} icon-large"></i>).html_safe, url, options
   end
+  def link_to_phone(icon, option, tel=nil)
+    link_to_icon icon, tel.present? ? "#{option}:#{tel}" : "javascript:void(0)", class: "btn #{'disabled' if tel.blank?}"
+  end
 end
