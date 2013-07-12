6a97c8629678480da04bd54dcff3f07c40f1e623
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-07-12 15:40:19 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -79,13 +79,15 @@ ActiveRecord::Schema.define(:version => 20130702175910) do
   add_index "survey_users", ["user_id"], :name => "index_survey_users_on_user_id"
 
   create_table "surveys", :force => true do |t|
+    t.integer  "activity_type_id"
     t.string   "title"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
-    t.integer  "activity_type_id"
     t.integer  "survey_id"
     t.integer  "campaign_id"
   end
+
+  add_index "surveys", ["activity_type_id"], :name => "index_surveys_on_activity_type_id"
 
   create_table "users", :force => true do |t|
     t.string   "email"
