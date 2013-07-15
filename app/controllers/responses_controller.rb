619bbf67d52304e5a34a81144d530390e552c080
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-07-15 17:47:59 -0400

    Prevent crash on nil

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -4,6 +4,8 @@ class ResponsesController < ApplicationController
     survey = Survey.find(params[:survey_id])
     @response = Response.find(:survey => survey, :id => params[:id])
     @lead = current_user.leads.find_by_response_id(params[:id])
-    @activities = @response.contact.activities.select { |a| a.status_id == Activity::STATUS_COMPLETED_ID }.reverse
+
+    @activities = @response.contact.activities || []
+    @activities = @activities.select { |a| a.status_id == Activity::STATUS_COMPLETED_ID }.reverse if @activities.present?
   end
 end
