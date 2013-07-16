dbdf022ab438c026431504f9da07e301e86486d2
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-07-16 13:13:52 -0400

    SD-53260099-User should be able to hide/show completed leads

--- app/helpers/connections_helper.rb
+++ app/helpers/connections_helper.rb
@@ -1,2 +1,9 @@
 module ConnectionsHelper
+  def lead_li_style(lead)
+    if lead.status_id == Lead::COMPLETED_STATUS_ID
+      'display: none;'
+    else
+      ''
+    end
+  end
 end
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -8,7 +8,10 @@ class Lead < ActiveRecord::Base
 
   scope :for_survey, lambda { |s| where(:survey_id => s.id) }
 
-  PROGRESS_STATUSES = [[4, 'Uncontacted'] ,[3, 'WIP'],[2, 'Completed']]
+  COMPLETED_STATUS_ID = 2
+  WIP_STATUS_ID = 3
+  UNCONTACTED_STATUS_ID = 4
+  PROGRESS_STATUSES = [[UNCONTACTED_STATUS_ID, 'Uncontacted'], [WIP_STATUS_ID, 'WIP'], [COMPLETED_STATUS_ID, 'Completed']]
 
   def status
     PROGRESS_STATUSES.select { |num|  num[0] == self.status_id  }.flatten[1]
