412032cae9f9834d542a9a6201cbb1c6dedf15b8
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-07-25 12:39:33 -0400

    Move some rejoiceable logic into model 2

--- app/models/rejoiceable.rb
+++ app/models/rejoiceable.rb
@@ -2,8 +2,8 @@ class Rejoiceable < Activity
   validates :custom_143, presence: true
 
   def initialize(attributes = {})
-    self.activity_type_id = ActivityType::REJOICEABLE_TYPE_ID
-    self.status_id = Lead::COMPLETED_STATUS_ID
+    attributes[:activity_type_id] = ActivityType::REJOICEABLE_TYPE_ID
+    attributes[:status_id] = Lead::COMPLETED_STATUS_ID
     super(attributes)
   end
 end
