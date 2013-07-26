1e55142f2e6c9fb942d30777a02dd659546410ce
Adrian Teh <adrian@codespore.com>
2013-07-26 15:31:15 -0400

    Fixes timestamp days ago calculation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
   def link_to_phone(icon, option, tel=nil)
     link_to_icon icon, tel.present? ? "#{option}:#{tel}" : "javascript:void(0)", class: "btn #{'disabled' if tel.blank?}"
   end
+
+  def custom_time_ago_in_words(time_str)
+    time = time_str.to_time + (-Time.zone_offset(Time.now.zone))
+    "#{time_ago_in_words(time)} ago"
+  end
+
+  def days_ago(date_str)
+    d = Date.parse(date_str)
+    days = Time.now.to_date - d
+    days.zero? ? 'Today' : "#{days} days ago"
+  end
 end
