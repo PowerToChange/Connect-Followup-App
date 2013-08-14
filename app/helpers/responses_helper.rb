ba240022ea627ed081868487fb68d885a369f661
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-08-14 16:26:07 -0400

    SD-54232368-Have a standard spot for the result of initial follow-up to be displayed for completed contacts.

--- app/helpers/responses_helper.rb
+++ app/helpers/responses_helper.rb
@@ -1,3 +1,11 @@
 module ResponsesHelper
   include ActivitiesHelper
+
+  def lead_report_description(lead)
+    if lead.completed? && lead.engagement_level.present?
+      "#{ Lead::REPORT_OUTCOMES_GROUPED_BY_ID[lead.engagement_level.to_i][:description].humanize }."
+    else
+      ''
+    end
+  end
 end
