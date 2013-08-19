6f8df13377cd3818a61ec81a9041b55b9ed8f816
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-08-19 15:58:48 -0400

    Tweak survey title for mobile; Remove surveys index

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,8 +1,5 @@
 class SurveysController < ApplicationController
   before_filter :authenticate_user!
-  def index
-    @surveys = Survey.all
-  end
 
   def show
     @survey = Survey.find(params[:id])
