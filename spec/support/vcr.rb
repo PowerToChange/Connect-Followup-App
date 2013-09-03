e6c35cebe301d699f5ab97beda9650b6d87f4b38
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-09-03 18:08:43 -0400

    Don't record new episode by default (this is causing wonkyness??)

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,7 +2,6 @@ VCR.configure do |c|
   c.cassette_library_dir  = Rails.root.join("spec", "vcr")
   c.hook_into :webmock
   c.ignore_localhost = true
-  c.default_cassette_options = { record: :new_episodes }
 end
 RSpec.configure do |c|
   c.treat_symbols_as_metadata_keys_with_true_values = true
