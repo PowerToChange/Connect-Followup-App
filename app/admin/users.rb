4c91a8e25dd9fd08ac0125639398ab2ffe1e2a05
Sheldon Dueck <sheldon.dueck@gmail.com>
2013-09-09 11:11:42 -0400

    Improvements to admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -7,6 +7,18 @@ ActiveAdmin.register User do
       end
     end
 
+    panel "Connections" do
+      table_for user.leads do
+        column "Survey" do |lead| lead.survey.title end
+        column "Activity ID" do |lead| lead.response_id end
+        column "Contact ID" do |lead| lead.contact_id end
+        column "Status" do |lead| lead.status end
+        column "Engagement Level" do |lead| engagement_level_label(lead.engagement_level) end
+        column "Created At" do |lead| lead.created_at end
+        column "Updated At" do |lead| lead.updated_at end
+      end
+    end
+
     panel "Associated Schools" do
       table_for user.schools.sort_by(&:display_name) do
         column "School Name" do |school|
