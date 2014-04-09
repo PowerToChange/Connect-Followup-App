173af1667aca01fde1ef80ddf504135ee2c10033
jacques <jacques.robitaille@gmail.com>
2014-04-09 09:35:37 -0400

    back to old environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,23 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 ConnectFollowup::Application.initialize!
 
-# enable detailed CAS logging 
-RAILS_ROOT = Rails.root.to_s unless defined? RAILS_ROOT
-
-cas_logger = CASClient::Logger.new(RAILS_ROOT+'/log/cas.log')
-
-cas_logger.level = Logger::DEBUG
-
-
 CASClient::Frameworks::Rails::Filter.configure(
-  :cas_base_url => "https://thekey.me/cas/",
-  :login_url     => "https://thekey.me/cas/login", 
-  :logout_url    => "https://thekey.me/cas/logout?service=http://p2c.com/students",
-  :logger => cas_logger,
-  :enable_single_sign_out => true
+  :cas_base_url => "https://thekey.me/cas/"
 )
-
-# Load the rails application
-
-require File.expand_path('../application', __FILE__)
-
